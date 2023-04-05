5e38e2d783396748857c162206e9db72aaca3f02
Pacificorp193 <GGreen58@s.tooeletech.edu.com>
2023-04-05 15:09:22 -0600

    first commit

---
+++ app/src/main/java/zappycode/example/versioncontroldemo/MainActivity.java
@@ -0,0 +1,18 @@
+package zappycode.example.versioncontroldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String name = "Nick";
+
+        // We love facebook
+        // Login now works
+    }
+}
